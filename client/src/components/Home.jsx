c0e279f19fa197658e448cc21829bf47ccbd9c8f
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-04-15 00:39:20 +0530

    Default values are set using react hooks

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useHistory } from 'react-router';
+import { UserContext } from '../context/UserContext'
 
 export default function Home() {
+    const {user,setUser} = useContext(UserContext);
+    const [room,setRoom] = useState("");
+    const [error,setError] = useState(false);
+    const history = useHistory();
+    
+    // if(user === null) {
+    //     history.push("/");
+    // }
     return (
         <div>
             Home
