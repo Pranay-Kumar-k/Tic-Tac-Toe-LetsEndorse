0e716bfd11d21354bfca9f2d203a49cb44d02b2a
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-04-15 00:13:07 +0530

    created context and routes given to home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import './App.css';
+import { UserContext } from "./context/UserContext";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
+  const [user,setUser] = React.useState(null);
   return (
-    <div className="App">
-     Tic-Tac-Toe
-    </div>
+    <Router>
+      <div className="App">
+        <UserContext.Provider value={{user,setUser}}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </UserContext.Provider>
+      </div>
+    </Router>
   );
 }
 
